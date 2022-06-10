5b7efb16dd61f461122989dc0b3610a822b5372b
sunny740 <sunnysejwarvitm005@gmail.com>
2022-06-11 01:30:42 +0530

    [Sunny] Add : Annotation Added

---
+++ AnnotationsDemo/Employee.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnnotationsDemo
+{
+    public class Employee
+    {
+        [Required(ErrorMessage = "Id Should be Mandatory")]
+        public int Id { get; set; }
+        [Required(ErrorMessage = "Employee {0} is Required")]
+        [StringLength(100, MinimumLength = 3, ErrorMessage = "Name Should be above 3 Char")]
+        [DataType(DataType.Text)]
+        public string Name { get; set; }
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+        [Range(18, 99, ErrorMessage = "Age Should Be Above 18")]
+        public int Age { get; set; }
+        [DataType(DataType.PhoneNumber)]
+        public long MobileNumber { get; set; }
+    }
+}
---
+++ AnnotationsDemo/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+using System.Threading.Tasks;
+
+namespace AnnotationsDemo
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            Employee employee = new Employee();
+            employee.Name = "Su";
+            employee.Id = 10123;
+            employee.Email = "Sej007@.com";
+            employee.Age = 23;
+            employee.MobileNumber = 9012345678;
+            Console.WriteLine(employee.Name);
+            Console.WriteLine(employee.Id);
+            Console.WriteLine(employee.Email);
+            Console.WriteLine(employee.Age);
+            Console.WriteLine(employee.MobileNumber);
+        }
+    }
+}
